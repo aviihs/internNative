1ade465cec7af2b8a088e5c8087707b973b2828c
Shiva Bhusal <srollins175@gmail.com>
2026-02-19 15:38:10 +0545

    Project Folder, Files Setup

---
+++ nativePaperDemo/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-blue-600 font-semibold text-lg">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
